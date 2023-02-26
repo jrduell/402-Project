58a683639fe7d04f7e8dceade19fb7f83e82316a
jacob-armiger <armigersemail@gmail.com>
2023-02-26 17:33:29 -0500

    Add components to index and remove feed.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import NavBar from 'components/NavigationBar'
+import SideBar from 'components/SideBar'
+import Post from 'components/Post'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        
+        <NavBar />
+        <SideBar/>
+        <Post />
       </main>
     </>
   )
