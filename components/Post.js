42c81a4da389599b99baa7c6988b1e6e2abf6fa3
John Allbritten <me@johnallbritten.com>
2023-02-27 00:10:19 -0500

    Update post styling

--- components/Post.js
+++ components/Post.js
@@ -2,16 +2,18 @@ import Link from 'next/link'
 
 export default function Post() {
     return (
-      <div className="w-72 bg-gray-400 border rounded-lg drop-shadow-md mb-6">
+      <div className="m-2 w-72 bg-gray-300 rounded-lg drop-shadow-md mb-6">
         {/* Link will need to be dynamically fetched based on post ID
         probably end up being something like post/{id} */}
         <Link href="/post">
-          <img className="rounded-t-md" src="test_img.png" />
-          <p>
-            This is a graph of my data. It look like this because of the way it
-            is.
-          </p>
-          <p className="text-right pr-1">User_Name</p>
+          <img className="rounded-t-lg" src="test_img.png" />
+          <div className="border-l border-b border-r rounded-t-none rounded-l-lg rounded-b-lg rounded-r-lg border-gray-400">
+            <p className="p-2">
+              This is a graph of my data. It look like this because of the way it
+              is.
+            </p>
+            <p className="p-2 text-right">User_Name</p>
+          </div>
         </Link>
       </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-// import styles from '@/styles/Home.module.css'
 import SideBar from 'components/SideBar'
 import Post from 'components/Post'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -16,11 +12,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
-        <SideBar/>
-        <Post />
-        <Post />
-        <Post />
+      <main className="flex">
+        <div>
+          <SideBar/>
+        </div>
+        <div className="bg-gray-100 p-4 flex flex-wrap overflow-scroll">
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
       </main>
     </>
   )
