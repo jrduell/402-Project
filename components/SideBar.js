4361074263fb8e9787fd557cee54ad46b453d2ec
John Allbritten <me@johnallbritten.com>
2023-02-27 00:10:41 -0500

    Fix sidebar position

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,8 +1,8 @@
 export default function SideBar() {
     return (
-        <div className="fixed h-screen w-52 bg-gray-400 p-4">
+        <div className="sticky h-screen w-52 bg-gray-200 border-r border-gray-400 p-4">
             <div className="items-center">
-                <h1>SIDEBAR</h1>
+                <h1 className="font-bold">SIDEBAR</h1>
                 <p>Subscription 1</p>
                 <p>Subscription 2</p>
                 <p>Subscription 3</p>
