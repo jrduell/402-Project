97952bfdcab8f920aa4c28cc850846ca1c53a74b
jacob-armiger <armigersemail@gmail.com>
2023-02-27 00:36:31 -0500

    Update sidebar to dynamically populate

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,12 +1,24 @@
+import Link from 'next/link'
+
 export default function SideBar() {
+    const subscriptions = [
+        "finance_guy27",
+        "REAL Stats",
+        "TNPrices",
+        "Stock Watchdog",
+        "Fortune500"
+      ];
+
     return (
-        <div className="sticky h-screen w-52 bg-gray-200 border-r border-gray-400 p-4">
-            <div className="items-center">
-                <h1 className="font-bold">SIDEBAR</h1>
-                <p>Subscription 1</p>
-                <p>Subscription 2</p>
-                <p>Subscription 3</p>
+      <div className="sticky h-screen w-52 border-r border-gray-400 bg-gray-200 p-4">
+        <div className="items-center">
+          <h1 className="flex flex-col font-bold">Subscriptions</h1>
+          {subscriptions.map((channel) => (
+            <div>
+              <Link href="account">{channel}</Link>
             </div>
+          ))}
         </div>
-    )
+      </div>
+    );
   }
