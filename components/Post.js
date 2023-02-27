ace7ef48ceb887d76b6bfcea2f23c000da49d147
jacob-armiger <armigersemail@gmail.com>
2023-02-26 19:20:59 -0500

    Update Post styling and add post page

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,19 @@
+import Link from 'next/link'
+
 export default function Post() {
     return (
-        <div className="max-w-sm bg-gray-400 border rounded-lg drop-shadow-md mb-6">
-            <img className="rounded-t-md" src='test_img.png'/>
-            <p>This is a graph of my data. It look like this because of the way it is.</p>
-            <p className="text-right pr-1">User_Name</p>
-            <p></p>
-        </div>
-    )
+      <div className="w-72 bg-gray-400 border rounded-lg drop-shadow-md mb-6">
+        {/* Link will need to be dynamically fetched based on post ID
+        probably end up being something like post/{id} */}
+        <Link href="/post"> 
+          <img className="rounded-t-md" src="test_img.png" />
+          <p>
+            This is a graph of my data. It look like this because of the way it
+            is.
+          </p>
+          <p className="text-right pr-1">User_Name</p>
+        </Link>
+      </div>
+    );
   }
 
