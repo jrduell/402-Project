f3c095c5376b854bf2be591e5d3bfc0829e2e4d5
jacob-armiger <armigersemail@gmail.com>
2023-02-26 19:02:10 -0500

    Update Post styling and add example img

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,9 @@
 export default function Post() {
     return (
-        <div className="h-56 w-56 bg-gray-400 p-4 rounded-lg drop-shadow-md mb-6">
-            <p>UserName</p>
-            <br></br>
-            <p>Image preview</p>
-            <br></br>
-            <br></br>
-            <br></br>
-            <p>Description</p>
+        <div className="max-w-sm bg-gray-400 border rounded-lg drop-shadow-md mb-6">
+            <img className="rounded-t-md" src='test_img.png'/>
+            <p>This is a graph of my data. It look like this because of the way it is.</p>
+            <p className="text-right pr-1">User_Name</p>
             <p></p>
         </div>
     )
