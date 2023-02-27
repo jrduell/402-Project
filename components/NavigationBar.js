4c41d1b981c3b445e24e2e9a5104e49c61cdbfc5
John Allbritten <me@johnallbritten.com>
2023-02-26 23:57:44 -0500

    Use React Links in navbar

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function NavBar() {
     const handleSearch = (event) => {
         console.log("Searching...")
@@ -6,18 +8,18 @@ export default function NavBar() {
     return (
       <div className="shadow-md mb-4 text-xl">
         <nav className="flex flex-wrap items-center mx-auto max-w-3xl rounded-b p-2">
-          <a className="py-1 my-auto rounded" href="/">
+          <Link className="py-1 my-auto rounded" href="/">
             <img className="ml-2 pr-2 mr-4 mt-auto mb-1" src="/logo-with-text.png" width="200"></img>
-          </a>
-          <a className="navbar-button" href="/">
+          </Link>
+          <Link className="navbar-button" href="/">
             Home
-          </a>
-          <a className="navbar-button" href="/login">
+          </Link>
+          <Link className="navbar-button" href="/login">
             Log In
-          </a>
-          <a className="navbar-button" href="/create">
+          </Link>
+          <Link className="navbar-button" href="/create">
             Create Account
-          </a>
+          </Link>
         </nav>
       </div>
     )
