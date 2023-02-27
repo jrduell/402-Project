70a9776a506a069c6d3f1ef5e644521ee11b9d9a
John Allbritten <me@johnallbritten.com>
2023-02-27 00:12:46 -0500

    Fix footer background

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 export default function NavBar() {
     return (
-      <div className="w-full text-center bg-gray-200 border-t border-gray-800">
-        <div className="my-4">
-          <p>DataSocial</p>
+      <div className="w-full p-4 text-center bg-gray-200 border-t border-gray-800">
+        <div>
+          <p>© 2023 DataSocial</p>
         </div>
       </div>
     )
